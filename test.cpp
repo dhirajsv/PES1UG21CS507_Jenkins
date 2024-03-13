71b66d8e7497bcd247eff91048fc3699f6e415ca
dhirajsv <73624805+dhirajsv@users.noreply.github.com>
2024-03-13 09:51:38 +0530

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2, sum;
+    num1=5;
+    num2=10;
+    cout<<"THIS IS TASK 5";
+    cout << "Enter the first number: ";
+    sum = num1 + num2;
+    cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << endl;
+    return 0;
+}
